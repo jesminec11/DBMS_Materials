c40de5fc8b5e62818274a26782673c2d8bd73c34
Jesmine Chaudhuri <jesminechaudhuri11@gmail.com>
2026-06-10 19:11:44 +0530

    more sqls

--- first.sql
+++ first.sql
@@ -26,3 +26,96 @@ SELECT
     unit_price,
     unit_price * 1.1 AS "new price"
 FROM products;
+
+SELECT * 
+FROM customers
+WHERE points > 3000;
+
+SELECT * 
+FROM customers
+WHERE state = "VA";
+
+SELECT * 
+FROM customers
+WHERE state != "VA";
+
+SELECT * 
+FROM customers
+WHERE state <> "VA";
+
+SELECT * 
+FROM customers
+WHERE birth_date > "1990-01-01";
+
+SELECT *
+FROM orders
+WHERE order_date >= "2019-01-01";
+
+SELECT * 
+FROM customers
+WHERE birth_date > "1990-01-01" AND points > 1000;
+
+SELECT * 
+FROM customers
+WHERE birth_date > "1990-01-01" OR points > 1000;
+
+SELECT * 
+FROM customers
+WHERE birth_date > "1990-01-01" OR 
+	  (points > 1000 AND state = "VA");
+      
+SELECT *
+FROM customers
+WHERE NOT (birth_date > "1990-01-01" OR points > 1000);
+
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND (unit_price * quantity) > 30;
+
+SELECT *
+FROM customers
+WHERE state IN ("VA", "GA", "FL");
+
+SELECT *
+FROM customers
+WHERE state NOT IN ("VA", "GA", "FL");
+
+SELECT *
+FROM products
+WHERE quantity_in_stock IN (49, 38, 72);
+
+SELECT *
+FROM customers
+WHERE points BETWEEN 1000 AND 3000;
+
+SELECT *
+FROM customers
+WHERE birth_date BETWEEN "1990-01-01" AND "2000-01-01";
+
+SELECT *
+FROM customers
+WHERE last_name LIKE "B%";
+
+SELECT *
+FROM customers
+WHERE last_name LIKE "%b%";
+
+SELECT *
+FROM customers
+WHERE last_name LIKE "%y";
+
+SELECT *
+FROM customers
+WHERE last_name LIKE "_y";
+
+SELECT *
+FROM customers
+WHERE last_name LIKE "b____y";
+
+SELECT *
+FROM customers
+WHERE address LIKE "%trail%" OR address LIKE "%avenue%";
+
+SELECT *
+FROM customers
+WHERE phone LIKE "%9";
